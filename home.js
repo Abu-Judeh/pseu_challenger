f377ed8defda14760a5b8885a0a69eeb9c2d945c
Abu-Judeh <muhammad-herbawi@hotmail.com>
2020-12-21 02:10:10 +0200

    button added

--- home.js
+++ home.js
@@ -14,3 +14,10 @@ function setGradient()
 
 
 }
+
+function go_To_Ask_Function() {
+  document.documentElement.scrollTop =  1100; // For Chrome, Firefox, IE and Opera
+}
+function go_To_Speedo_Function() {
+  document.documentElement.scrollTop =  2200; // For Chrome, Firefox, IE and Opera
+}
